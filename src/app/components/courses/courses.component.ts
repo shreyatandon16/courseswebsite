99a6a075fd1add8668e8546066d3f0c05f0abb45
Sathvik <satviklca@gmail.com>
2023-10-10 21:53:05 +0530

    Added grid to the courses

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
+
+  courses: any;
+
+  constructor(){
+    this.courses = [
+      {courseName: 'Python Course', courseImage: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages3.alphacoders.com%2F110%2F1108591.jpg&f=1&nofb=1&ipt=2615c263ad811b160000c98c1fcd13e8dc5b0ea94803b4544b516594e54e0924&ipo=images'},
+      {courseName: 'Python Course', courseImage: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages3.alphacoders.com%2F110%2F1108591.jpg&f=1&nofb=1&ipt=2615c263ad811b160000c98c1fcd13e8dc5b0ea94803b4544b516594e54e0924&ipo=images'},
+      {courseName: 'Python Course', courseImage: 'courseImageUrl'},
+      {courseName: 'Python Course', courseImage: 'courseImageUrl'},
+      {courseName: 'Python Course', courseImage: 'courseImageUrl'},
+    ]
+  }
 }
